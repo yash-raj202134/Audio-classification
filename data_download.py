45183f9c8ff21762d60d8841852318f0a1427993
yash-raj202134 <yashraj3376@gmail.com>
2024-04-01 19:41:35 +0530

    requirements setup

---
+++ data_download.py
@@ -0,0 +1,8 @@
+import soundata
+
+dataset = soundata.initialize('urbansound8k')
+dataset.download()  # download the dataset
+dataset.validate()  # validate that all the expected files are there
+
+example_clip = dataset.choice_clip()  # choose a random example clip
+print(example_clip)  # see the available data
