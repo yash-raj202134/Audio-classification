87758bab7f7c82ea39a8ac36d04a6176258d39fb
yash-raj202134 <yashraj3376@gmail.com>
2024-04-03 14:15:58 +0530

    utils

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # Testing
+import sys
+import os
+from src.audioclf.logger import logging
+from src.audioclf.exception import CustomException
+
+
+
+# logging.info("testing log")
+
+# try:
+#     1/0
+# except Exception as e:
+#     raise CustomException(e,sys)
+
